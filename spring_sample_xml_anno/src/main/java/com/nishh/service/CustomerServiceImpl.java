0b691dc725d50dace7f8564626563eeaa9c2c33d
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-09-12 20:57:30 +0530

    doing setter injection

--- spring_sample_xml_anno/src/main/java/com/nishh/service/CustomerServiceImpl.java
+++ spring_sample_xml_anno/src/main/java/com/nishh/service/CustomerServiceImpl.java
@@ -11,12 +11,19 @@ import com.nishh.repository.CustomerRepository;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-	// private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
-	@Autowired
+	// private CustomerRepository customerRepository = new
+	// HibernateCustomerRepositoryImpl();
+
 	private CustomerRepository customerRepository;
 
 	@Override
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
+	}
+   
+	@Autowired
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		System.out.println("we are doing setter injection");
+		this.customerRepository = customerRepository;
 	}
 }
