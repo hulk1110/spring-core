a17fa2daff93f9ae5959f0bcc9f3b7cb5dd2f9b6
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-09-12 20:54:57 +0530

    correcting schema definition for app and demo for member injecion demo

--- spring_sample_xml_anno/src/main/java/com/nishh/service/CustomerServiceImpl.java
+++ spring_sample_xml_anno/src/main/java/com/nishh/service/CustomerServiceImpl.java
@@ -2,16 +2,18 @@ package com.nishh.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nishh.model.Customer;
 import com.nishh.repository.CustomerRepository;
-import com.nishh.repository.HibernateCustomerRepositoryImpl;
 
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+	// private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+	@Autowired
+	private CustomerRepository customerRepository;
 
 	@Override
 	public List<Customer> findAll() {
