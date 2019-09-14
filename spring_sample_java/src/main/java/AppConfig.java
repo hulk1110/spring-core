af20f3a218c5ff1d971a9350fe1143d8521a3a92
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-09-14 08:31:43 +0530

    use of autowire and @Service, we have removed everything from the body of app config

--- spring_sample_java/src/main/java/AppConfig.java
+++ spring_sample_java/src/main/java/AppConfig.java
@@ -1,23 +1,18 @@
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.nishh.repository.CustomerRepository;
-import com.nishh.repository.HibernateCustomerRepositoryImpl;
-import com.nishh.service.CustomerService;
-import com.nishh.service.CustomerServiceImpl;
 
 @Configuration
 @ComponentScan({"com.nishh"})
 public class AppConfig {
 
-	@Bean(name = "customerService")
-	public CustomerService getCustomerService() {
-		// CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl(getCustomerRepository());
-		CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
-		// customerServiceImpl.setCustomerRepository(getCustomerRepository());
-		return customerServiceImpl;
-	}
+	/*
+	 * @Bean(name = "customerService") public CustomerService getCustomerService() {
+	 * // CustomerServiceImpl customerServiceImpl = new
+	 * CustomerServiceImpl(getCustomerRepository()); CustomerServiceImpl
+	 * customerServiceImpl = new CustomerServiceImpl(); //
+	 * customerServiceImpl.setCustomerRepository(getCustomerRepository()); return
+	 * customerServiceImpl; }
+	 */
 
 	/*
 	 * @Bean(name = "customerRepository") public CustomerRepository
--- spring_sample_java/src/main/java/com/nishh/service/CustomerServiceImpl.java
+++ spring_sample_java/src/main/java/com/nishh/service/CustomerServiceImpl.java
@@ -3,10 +3,12 @@ package com.nishh.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.nishh.model.Customer;
 import com.nishh.repository.CustomerRepository;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
 	private CustomerRepository customerRepository;
